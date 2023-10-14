7584c5a4af5dcb4f693443c7b781b9b6147f0658
wredwards94 <wredwards94@outlook.com>
2023-10-14 01:08:13 -0400

    completed assignment

--- client/src/app/inspect/inspect.component.ts
+++ client/src/app/inspect/inspect.component.ts
@@ -1,6 +1,22 @@
 import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
 import { UserService } from 'src/user.service';
 
+interface EmptyUser {
+  username: string,
+  name: string,
+  location: string,
+  bio: string,
+  avatar_url: string,
+  titles: [],
+  'favorite-language': string,
+  'public-repos': number,
+  'total-stars': number,
+  'highest-starred': number,
+  'perfect-repos': number,
+  followers: number,
+  following: number
+}
+
 @Component({
   selector: 'app-inspect',
   templateUrl: './inspect.component.html',
@@ -9,7 +25,22 @@ import { UserService } from 'src/user.service';
 export class InspectComponent implements OnInit {
 
   username: string = ""
-  userData: any
+  userData: EmptyUser = {
+    username: '',
+    name: '',
+    location: '',
+    bio: '',
+    avatar_url: '',
+    titles: [],
+    'favorite-language': '',
+    'public-repos': 0,
+    'total-stars': 0,
+    'highest-starred': 0,
+    'perfect-repos': 0,
+    followers: 0,
+    following: 0
+  }
+
   errorMessage: string = ''
   isUserVisible: boolean = false;
 
@@ -25,7 +56,7 @@ export class InspectComponent implements OnInit {
   onSubmit() {
     this.userService.inspectUser(this.username)
     .then(data => {
-      this.userData = data
+      this.userData = data as EmptyUser
       this.isUserVisible = true
     })
     .catch(error => {
